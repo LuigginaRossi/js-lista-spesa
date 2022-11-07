16884f6b919806a399cc53ff6d9773cb56e20ae9
LuigginaRossi <luigginavaldivia@gmail.com>
2022-11-07 20:19:00 +0100

    prove create element

--- js/file.js
+++ js/file.js
@@ -14,5 +14,31 @@ while ( i < listaSpesa.length ) {
     console.log( listaSpesa[ i ] );
     i++;
 
+    
+    const listaEl = document.querySelector( ".lista" );
+    console.log(listaEl);
+
+    //creiamo elementi (li):
+    const pluto = document.createElement('p');
+
+    //inseriamo qualcosa dentro li:
+    pluto.innerHTML +=  listaSpesa[ i ] ;
+
+    //stampiamo elemento completo:
+    listaEl.append(pluto);
+
 }
 
+//---------------------
+
+//creiamo elemento con createElement:
+const pippo = document.createElement('p');
+
+//inseriamo contenuto nell'elemento:
+pippo.innerHTML = "hello world";
+
+//stampiamo elemento completo:
+spesaEl.append(pippo);
+
+
+//---------------------
